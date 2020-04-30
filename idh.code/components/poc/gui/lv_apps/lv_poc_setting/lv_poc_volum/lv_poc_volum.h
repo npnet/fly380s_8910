ff042189e54ea158b471627e474df1f0b7b3adcf
lugj <lugj@flyscale.cn>
2020-04-30 16:55:16 +0800

    add slider on all ui that display level of volum

--- idh.code/components/poc/gui/lv_apps/lv_poc_setting/lv_poc_volum/lv_poc_volum.h
+++ idh.code/components/poc/gui/lv_apps/lv_poc_setting/lv_poc_volum/lv_poc_volum.h
@@ -1,11 +1,15 @@
 #ifndef __LV_POC_VOLUM_H_
 #define __LV_POC_VOLUM_H_
 
+#include "lv_include/lv_poc_type.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void lv_poc_volum_open(void);
+
+bool lv_poc_set_volum(POC_MMI_VOICE_TYPE_E type, uint8_t volume, bool play, bool display);
 
 #ifdef __cplusplus
 }
