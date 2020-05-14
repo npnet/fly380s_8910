3e1de259d424ae72d104172a438d7768ae70de50
lugj <lugj@flyscale.cn>
2020-05-14 16:14:35 +0800

    成员列表的结构改为指定具体的成员列表对象进行操作

--- idh.code/components/poc/gui/lv_apps/lv_poc_member_list/lv_poc_member_list.h
+++ idh.code/components/poc/gui/lv_apps/lv_poc_member_list/lv_poc_member_list.h
@@ -19,29 +19,29 @@ typedef struct {
 
 void lv_poc_member_list_open(IN char * title, IN lv_poc_member_list_t *members, IN bool hide_offline);
 
-lv_poc_status_t lv_poc_member_list_add(const char * name, bool is_online, void * information);
+lv_poc_status_t lv_poc_member_list_add(lv_poc_member_list_t *member_list_obj, const char * name, bool is_online, void * information);
 
-void lv_poc_member_list_remove(const char * name, void * information);
+void lv_poc_member_list_remove(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-void lv_poc_member_list_clear(void);
+void lv_poc_member_list_clear(lv_poc_member_list_t *member_list_obj);
 
-int lv_poc_member_list_get_information(const char * name, void *** information);
+int lv_poc_member_list_get_information(lv_poc_member_list_t *member_list_obj, const char * name, void *** information);
 
-void lv_poc_member_list_refresh(void);
+void lv_poc_member_list_refresh(lv_poc_member_list_t *member_list_obj);
 
-lv_poc_status_t lv_poc_member_list_move_top(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_move_top(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-lv_poc_status_t lv_poc_member_list_move_bottom(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_move_bottom(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-lv_poc_status_t lv_poc_member_list_move_up(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_move_up(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-lv_poc_status_t lv_poc_member_list_move_down(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_move_down(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-lv_poc_status_t lv_poc_member_list_set_state(const char * name, void * information, bool is_online);
+lv_poc_status_t lv_poc_member_list_set_state(lv_poc_member_list_t *member_list_obj, const char * name, void * information, bool is_online);
 
-lv_poc_status_t lv_poc_member_list_is_exists(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_is_exists(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
-lv_poc_status_t lv_poc_member_list_get_state(const char * name, void * information);
+lv_poc_status_t lv_poc_member_list_get_state(lv_poc_member_list_t *member_list_obj, const char * name, void * information);
 
 void lv_pov_member_list_get_list_cb(int msg_type);
 
